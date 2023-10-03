cf4f6fe067882573e9aa768c63bc1c7b206fc374
LucasZibaitis <zibaitislucas@gmail.com>
2023-10-03 19:33:15 -0300

    PI Models v1

---
+++ api/src/models/Genre.js
@@ -0,0 +1,13 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define("genre", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    name: { type: DataTypes.STRING, allowNull: false },
+  });
+};
--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,21 @@
-const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+const { DataTypes } = require("sequelize");
+
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('videogame', {
+  sequelize.define("videogame", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description: { type: DataTypes.TEXT("long"), allowNull: false },
+    platforms: { type: DataTypes.ARRAY(DataTypes.STRING), allowNull: false },
+    image: { type: DataTypes.STRING, allowNull: false },
+    releaseDate: { type: DataTypes.DATE, allowNull: false },
+    rating: { type: DataTypes.INTEGER, allowNull: false },
   });
 };
