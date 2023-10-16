715e308a8abc3f2706aee790cd8383f339c413ef
LucasZibaitis <zibaitislucas@gmail.com>
2023-10-16 19:25:05 -0300

    PI Styles v2

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./Nav.module.css";
+import navImg from "./Nav.png";
 
-export default function Nav(props) {
+export default function Nav() {
   return (
     <div>
+      <img src={navImg} className={styles.navBarImg} />
       <Link to="/home">
-        <button>Home</button>
+        <p className={styles.navBarButtonHome}>home</p>
       </Link>
       <Link to="/form">
-        <button>Create Videogame</button>
+        <p className={styles.navBarButtonForm}>create videogame</p>
       </Link>
       <Link to="/">
-        <button>Log Out</button>
+        <p className={styles.navBarButtonExit}>exit</p>
       </Link>
     </div>
   );
