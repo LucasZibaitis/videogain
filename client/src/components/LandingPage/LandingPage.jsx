fed3f69cd4b29faf43b0e751325e16aee031d610
LucasZibaitis <zibaitislucas@gmail.com>
2024-01-06 19:07:49 -0300

    favicon y metadata + responsive landing

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import LandingLogo1 from "../LandingPage/LandingPageLogo1.png";
 import LandingLogo2 from "../LandingPage/LandingPageLogo2.png";
+import MediaQuery from "react-responsive";
 
 import styles from "./LandingPage.module.css";
 
 export default function LandingPage(props) {
   const [isHovered, setIsHovered] = React.useState(false);
-
   const handleMouseEnter = () => {
     setIsHovered(true);
   };
@@ -20,11 +20,16 @@ export default function LandingPage(props) {
     <div>
       <Link to="/home">
         <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-          <img
-            className={styles.Logo}
-            src={isHovered ? LandingLogo2 : LandingLogo1}
-            width={600}
-          />
+          <MediaQuery maxWidth={600}>
+            <img className={styles.Logo} src={LandingLogo2} width={350} />
+          </MediaQuery>
+          <MediaQuery minWidth={601}>
+            <img
+              className={styles.Logo}
+              src={isHovered ? LandingLogo2 : LandingLogo1}
+              width={600}
+            />
+          </MediaQuery>
         </div>
       </Link>
     </div>
