43119a937f7284abb99d511e9a9dc80a08cb995b
LucasZibaitis <128875825+LucasZibaitis@users.noreply.github.com>
2024-02-09 22:19:06 -0300

    Update getGenres.js

--- api/src/controllers/getGenres.js
+++ api/src/controllers/getGenres.js
@@ -8,18 +8,18 @@ const getGenres = async (req, res) => {
     const { data } = await axios.get(URL);
     const genresFromAPI = data.results;
 
-    for (const genreData of genresFromAPI) {
-      const existingGenre = await Genre.findOne({
-        where: { id: genreData.id },
-      });
+    // for (const genreData of genresFromAPI) {
+    //   const existingGenre = await Genre.findOne({
+    //     where: { id: genreData.id },
+    //   });
 
-      if (!existingGenre) {
-        await Genre.create({
-          id: genreData.id,
-          name: genreData.name,
-        });
-      }
-    }
+    //   if (!existingGenre) {
+    //     await Genre.create({
+    //       id: genreData.id,
+    //       name: genreData.name,
+    //     });
+    //   }
+    // }
     return res.status(200).send(genresFromAPI);
   } catch (error) {
     return res.status(500).json({ error: error.message });
